f0c17bff5257101084f5d3c08ac4a7c367592d01
volo367 <147878813+volo367@users.noreply.github.com>
2024-12-29 20:45:29 +0100

    Actualizacion Volo esPalindromo.java

--- src/Actividad1/esPalindromo.java
+++ src/Actividad1/esPalindromo.java
@@ -1,5 +1,5 @@
 package Actividad1;
-
+//Volodymyr Vatamanyuk Parashchuk
 public class esPalindromo {
 	public static boolean validPalindrome(String s) {
 	       for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
@@ -27,3 +27,4 @@ public class esPalindromo {
 
 	   }
 }
+//Volodymyr Vatamanyuk Parashchuk
